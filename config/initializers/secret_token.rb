ecfe1eb750d6ca80af9b3b3b813e62a88555d0ca
cokeefe <chaemobile@gmail.com>
2013-05-11 10:51:10 -0400

    scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsgirlsprojectsWorkshop::Application.config.secret_token = 'ac8f2ff73b3092ff74fba71d39d44aee414325ee3142f2b3d766ddde76ecf86dbcd419f9c40c043152ef87c783bbb2468e8ca3a451afff2eca7f0ddc94d5714a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsgirlsprojectsWorkshop::Application.config.session_store :cookie_store, key: '_railsgirlsprojects_workshop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsgirlsprojectsWorkshop::Application.config.session_store :active_record_store
